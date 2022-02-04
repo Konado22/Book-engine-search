7eaf1555946a43897ade809d35497f916d62aa95
unknown <jessedraper2@gmail.com>
2022-02-04 13:31:38 -0700

    started adding querries and routes in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,20 @@
 const {Book, User} = require('../models')
+const { authMiddleware } = require('../../utils/auth');
 //querries
 const resolvers ={
     Query: {},
-    Mutations: {}
+    Mutations: {
+        createUser: async () => {
+            const newUser = await User.create(args)
+            return newUser
+        },
+        login: async () => {
+
+        },
+    getSingleUser: async () =>{
+        const getSingleUser = await User.findOne(
+            {authMiddleware}
+        )
+    }}
 }
 module.exports = resolvers;
