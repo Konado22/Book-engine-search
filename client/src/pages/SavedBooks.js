7a5730a40f248678a337a77d1f324e7651815b07
unknown <jessedraper2@gmail.com>
2022-02-10 12:09:05 -0700

    heroku

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,35 +4,37 @@ import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { removeBookId, saveBookIds } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const {loading, data} = useQuery(GET_ME)
   const userData = data?.matchups || [];
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  // const handleDeleteBook = async (bookId) => {
-  //   const [deleteBook, {error}] = useMutation(REMOVE_BOOK); 
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const handleDeleteBook = async (bookId) => {
+    const [deleteBook, {error}] = useMutation(REMOVE_BOOK); 
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //   if (!token) {
-  //     return false;
-  //   }
+    if (!token) {
+      return false;
+    }
 
-  //   try {
-  //     const response = await deleteBook(bookId, token);
+    try {
+      const {data} = await deleteBook({
+        variables: {bookId}
+      });
 
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
 
-  //     const updatedUser = await response.json();
-  //     setUserData(updatedUser);
-  //     // upon success, remove book's id from localStorage
-  //     removeBookId(bookId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      const updatedUser = await response.json();
+      setUserData(updatedUser);
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   // if data isn't here yet, say so
   if (!userDataLength) {
