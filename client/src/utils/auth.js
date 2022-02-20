6c205a647c99f237c2467934e462057b6f90dfe3
unknown <jessedraper2@gmail.com>
2022-02-20 15:52:56 -0700

    heroku try again

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,4 +1,5 @@
 // use this to decode a token and get the user's information out of it
+const jwt = require('jsonwebtoken');
 import decode from 'jwt-decode';
 
 // create a new class to instantiate for a user
